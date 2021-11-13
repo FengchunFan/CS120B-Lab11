6d82e9336b2523fac99bba5a3f4f90a22df7c319
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-11-12 20:14:53 -0800

    Update ffan005_lab11_part1.c

--- ffan005_lab11_part1.c
+++ ffan005_lab11_part1.c
@@ -44,58 +44,40 @@ int Tick(int state){
                 case Init:
                         if(input == '\0'){
                                 output = 0x1F;
-                                break;
                         }else if (input == '1'){
                                 output = 0x01;
-                                break;
                         }else if (input == '2'){
                                 output = 0x02;
-                                break;
                         }else if (input == '3'){
                                 output = 0x03;
-                                break;
                         }else if (input == '4'){
                                 output = 0x04;
-                                break;
                         }else if (input == '5'){
                                 output = 0x05;
-                                break;
                         }else if (input == '6'){
                                 output = 0x06;
-                                break;
                         }else if (input == '7'){
                                 output = 0x07;
-                                break;
                         }else if (input == '8'){
                                 output = 0x08;
-                                break;
-                        }else if (input == '9'){
+                        }else if (input == '9'){                   
                                 output = 0x09;
-                                break;
                         }else if (input == 'A'){
                                 output = 0x0A;
-                                break;
                         }else if (input == 'B'){
                                 output = 0x0B;
-                                break;
                         }else if (input == 'C'){
                                 output = 0x0C;
-                                break;
                         }else if (input == 'D'){
                                 output = 0x0D;
-                                break;
                         }else if (input == '*'){
                                 output = 0x0E;
-                                break;
                         }else if (input == '#'){
                                 output = 0x0F;
-                                break;
                         }else if (input == '0'){
                                 output = 0x00;
-                                break;
                         }else {
                                 output = 0x1B;
-                                break;
                         }
                         PORTB = output;
                         break;
