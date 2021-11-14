857b66259f66c0630eaee8de53ad6a29baca0b50
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-11-13 17:22:27 -0800

    Update ffan005_lab11_part1.c

--- ffan005_lab11_part1.c
+++ ffan005_lab11_part1.c
@@ -6,7 +6,7 @@
  *
  *      I acknowledge all content contained herein, excluding template or example
  *      code, is my own original work.
- *      Demo: https:
+ *      Demo: https: https://www.youtube.com/watch?v=0j31kiZaNEA
  */
 
 #include <avr/io.h>
