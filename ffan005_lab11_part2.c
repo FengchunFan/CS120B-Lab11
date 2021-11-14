4d5dce5e29799c829651ef1f877b44989c97eee3
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-11-13 17:48:43 -0800

    Update ffan005_lab11_part2.c

--- ffan005_lab11_part2.c
+++ ffan005_lab11_part2.c
@@ -1,1 +1,88 @@
+/*      Author: ffan005
+ *  Partner(s) Name: 
+ *      Lab Section:
+ *      Assignment: Lab #  Exercise #
+ *      Exercise Description: [optional - include for your own benefit]
+ *
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
+ *      Demo: 
+ */
+
+#include <avr/io.h>
+#include "io.h"
+#include <avr/interrupt.h>
+#include <timer.h>
+#include <keypad.h>
+#include <scheduler.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start,Init};
+unsigned char MSG[40]={'C','S','1','2','0','B',' ','i','s',' ','L','e','g','e','n','d','.','.','.','w','a','i','t',' ','f','o','r',' ','i','t',' ','D','A','R','Y','!',' ',' ',' ',' '};
+unsigned char i = 0x00;
+unsigned char lcd[16];
+
+int Tick(int state){
+        switch(state){
+                case Start:
+                        state = Init;
+                case Init:
+                        state = Init;
+                default:
+                        state = Start;
+        }
+
+        switch(state){
+                case Start:
+                        break;
+                case Init:
+                        for (int k = 0; k < 16; ++k) {
+                                        lcd[k] = MSG[(k + i) % 40];
+                                }
+                                i = ((i + 1) % 40);
+                                LCD_DisplayString(1, lcd);
+                                break;
+                default:
+                        break;
+        }
+
+        return state;
+}
+
+int main(void){
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xF0; PORTC = 0x0F;
+    DDRD = 0xFF; PORTD = 0x00;
+
+    LCD_init();
+
+    static task task1;
+    task *tasks[] = { &task1 };
+    const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
+    const char start = -1;
+
+    task1.state = start;
+    task1.period = 50;
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &Tick;
+
+    TimerSet(50);
+    TimerOn();
+
+    unsigned short i;
+    while (1) {
+        for (i = 0; i < numTasks; ++i) {
+                if (tasks[i]->elapsedTime == tasks[i]->period) {
+                        tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+                        tasks[i]->elapsedTime = 0;
+                }
+                tasks[i]->elapsedTime += 50;
+        }
+        while(!TimerFlag);
+        TimerFlag = 0;
+    }
+    return 1;
+}
 
